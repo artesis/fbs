d2dffffc939ab23db40eba19289407d4c3e17fc6
Nicolae Grigoriu <ng@inlead.dk>
2018-04-23 14:58:16 +0300

    DBC-1400 - Use v3 of fbs lib.

--- src/Model/UpdatePatronRequestV3.php
+++ src/Model/UpdatePatronRequestV3.php
@@ -1,14 +1,10 @@
 <?php
-
 namespace FBS\Model;
-
 class UpdatePatronRequestV3 {
-
   /**
    * @var PatronSettingsV3 Set this if patron details are to be changed
    */
   public $patron = null;
-
   /**
    * @var PincodeChange Set this if pincode is to be changed
    */
